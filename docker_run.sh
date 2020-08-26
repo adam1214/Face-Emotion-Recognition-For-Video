846641723fd009b31e0aeb40cf661b23fbaed2dc
adam1214 <f64051114@gs.ncku.edu.tw>
2020-08-26 10:46:02 +0800

    edit the name of docker image

--- docker_run.sh
+++ docker_run.sh
@@ -10,7 +10,7 @@ then
     -v ${FER_ROOT}/fer_finished:/media/$PROJECT_NAME/fer_finished \
     -v ${FER_ROOT}/fer_model:/media/$PROJECT_NAME/fer_model \
     -v ${FER_ROOT}/fer_result:/media/$PROJECT_NAME/fer_result \
-    fer_batch_processing \
+    biicgitlab.ee.nthu.edu.tw:5050/prod/engineer/fer_offline \
     /bin/bash -c "python3 main.py fer_input/ fer_output/ fer_result/ fer_finished/ fer_model/ 720p"
 
 else
@@ -21,6 +21,6 @@ else
     -v ${FER_ROOT}/fer_finished:/media/$PROJECT_NAME/fer_finished \
     -v ${FER_ROOT}/fer_model:/media/$PROJECT_NAME/fer_model \
     -v ${FER_ROOT}/fer_result:/media/$PROJECT_NAME/fer_result \
-    fer_batch_processing \
+    biicgitlab.ee.nthu.edu.tw:5050/prod/engineer/fer_offline \
     /bin/bash -c "python3 main.py fer_input/$1 fer_output/$1 fer_result/$1 fer_finished/$1 fer_model/ 720p"
 fi
