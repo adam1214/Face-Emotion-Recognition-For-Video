84d26326c74bae80142e94c336727a49da0457c5
adam1214 <f64051114@gs.ncku.edu.tw>
2020-09-02 20:58:17 +0800

    edit docker_run.bat

--- docker_run.bat
+++ docker_run.bat
@@ -2,26 +2,25 @@
 set FER_ROOT="%cd%"
 set PROJECT_NAME="fer_batch_processing"
 
-if [%1] == [] (
+IF [%1] == [] (
 	echo NO arguments passed
-	docker run -ti --rm  --name fer_batch_processing \
-	-v ${FER_ROOT}/fer_input:/media/$PROJECT_NAME/fer_input \
-	-v ${FER_ROOT}/fer_output:/media/$PROJECT_NAME/fer_output \
-	-v ${FER_ROOT}/fer_finished:/media/$PROJECT_NAME/fer_finished \
-	-v ${FER_ROOT}/fer_model:/media/$PROJECT_NAME/fer_model \
-	-v ${FER_ROOT}/fer_result:/media/$PROJECT_NAME/fer_result \
-	biicgitlab.ee.nthu.edu.tw:5050/prod/engineer/fer_offline \
+	docker run -ti --rm ^
+	-v %FER_ROOT%/fer_input:/media/%PROJECT_NAME%/fer_input ^
+	-v %FER_ROOT%/fer_output:/media/%PROJECT_NAME%/fer_output ^
+	-v %FER_ROOT%/fer_finished:/media/%PROJECT_NAME%/fer_finished ^
+	-v %FER_ROOT%/fer_model:/media/%PROJECT_NAME%/fer_model ^
+	-v %FER_ROOT%/fer_result:/media/%PROJECT_NAME%/fer_result ^
+	biicgitlab.ee.nthu.edu.tw:5050/prod/engineer/fer_offline:latest ^
 	/bin/bash -c "python3 main.py fer_input/ fer_output/ fer_result/ fer_finished/ fer_model/ 720p"
-	)
-else	(
+	) ELSE	(
 	echo Arguments passed
-	docker run -ti --rm  --name fer_batch_processing \
-	-v ${FER_ROOT}/fer_input:/media/$PROJECT_NAME/fer_input \
-	-v ${FER_ROOT}/fer_output:/media/$PROJECT_NAME/fer_output \
-	-v ${FER_ROOT}/fer_finished:/media/$PROJECT_NAME/fer_finished \
-	-v ${FER_ROOT}/fer_model:/media/$PROJECT_NAME/fer_model \
-	-v ${FER_ROOT}/fer_result:/media/$PROJECT_NAME/fer_result \
-	biicgitlab.ee.nthu.edu.tw:5050/prod/engineer/fer_offline \
+	docker run -ti --rm  ^
+	-v %FER_ROOT%/fer_input:/media/%PROJECT_NAME%/fer_input ^
+	-v %FER_ROOT%/fer_output:/media/%PROJECT_NAME%/fer_output ^
+	-v %FER_ROOT%/fer_finished:/media/%PROJECT_NAME%/fer_finished ^
+	-v %FER_ROOT%/fer_model:/media/%PROJECT_NAME%/fer_model ^
+	-v %FER_ROOT%/fer_result:/media/%PROJECT_NAME%/fer_result ^
+	biicgitlab.ee.nthu.edu.tw:5050/prod/engineer/fer_offline:latest ^
 	/bin/bash -c "python3 main.py fer_input/%1 fer_output/%1 fer_result/%1 fer_finished/%1 fer_model/ 720p"
 	)
 pause
