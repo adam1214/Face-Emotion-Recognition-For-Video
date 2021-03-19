69a702addf8083e75786fe490d8a41b5ecb9044d
adam1214 <f64051114@gs.ncku.edu.tw>
2021-03-19 13:39:23 +0800

    cd /media

--- docker_run.sh
+++ docker_run.sh
@@ -6,12 +6,12 @@ then
     docker run --rm \
     -v ${FER_ROOT}:/media \
     biicgitlab.ee.nthu.edu.tw:5050/prod/engineer/fer_offline \
-    /bin/bash -c "cd media; python3 main.py fer_input/ fer_result/ fer_finished/ fer_model/ 720p compress.zip"
+    /bin/bash -c "cd /media; python3 main.py fer_input/ fer_result/ fer_finished/ fer_model/ 720p compress.zip"
 
 else
 	echo "Arguments supplied"
     docker run --rm \
     -v ${FER_ROOT}:/media \
     biicgitlab.ee.nthu.edu.tw:5050/prod/engineer/fer_offline \
-    /bin/bash -c "cd media; python3 main.py fer_input/$1 fer_result/$1 fer_finished/$1 fer_model/ 720p compress.zip"
+    /bin/bash -c "cd /media; python3 main.py fer_input/$1 fer_result/$1 fer_finished/$1 fer_model/ 720p compress.zip"
 fi
